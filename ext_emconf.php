193d7195a00ee7ec9e80db093e91d3d18119a7b0
NITSAN <sanjay@nitsan.in>
2025-06-25 14:04:35 +0530

    Update ext_emconf.php
    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,13 @@
 <?php
 
 $EM_CONF['ns_gridtocontainer'] = [
-    'title' => 'Gridelements to Container',
-    'description' => "Are you tired of waiting for a public version of Gridelements for the current TYPO3 version? With EXT:container, there's a public solution available for creating grids and containers in TYPO3. We've developed a developer-friendly TYPO3 extension called EXT:ns_gridtocontainer to help you migrate from EXT:gridelements to this new extension. By using our TYPO3 extension, you can easily migrate and save yourself time and effort. 
+    'title' => 'Migrate Gridelements to Container',
+    'description' => "Quickly migrate from EXT:gridelements to EXT:container in TYPO3 with this developer-friendly tool. Save time and ensure compatibility with the latest TYPO3 versions using this streamlined migration extension.", 
     
-    *** Live-Demo: https://demo.t3planet.com/t3-extensions/typo3-gridelements-container *** Documentation & Free Support: https://t3planet.com/typo3-gridelements-container",
     'category' => 'plugin',
-    'author' => 'T3: Nilesh Malankiya, T3: Jay Bhalgamiya, QA: Krishna Dhapa',
-    'author_email' => 'sanjay@nitsan.in',
-    'author_company' => 'T3Planet // NITSAN',
+    'author' => 'Team T3Planet',
+    'author_email' => 'info@t3planet.de',
+    'author_company' => 'T3Planet',
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
     'version' => '2.0.0',
